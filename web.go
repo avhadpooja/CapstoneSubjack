688d204e53c3334ad2b3cf49fc0e02f893f3e329
avhadpooja <36248454+avhadpooja@users.noreply.github.com>
2018-03-05 15:20:15 -0500

    Update web.go

--- web.go
+++ web.go
@@ -1,8 +1,9 @@
-/*package main
+package main
 
 import (
 	"fmt"
 	"net/http"
+	"io/ioutil"
 	"os"
 )
 
@@ -12,52 +13,13 @@ func main() {
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		panic(err)
+	b, err := ioutil.ReadFile("file.txt") // just pass the file name
+    	if err != nil {
+        fmt.Print(err)
 	}
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "hello, heroku")
-}*/
-
-package main
-
-import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-)
-
-func main() {
-	http.HandleFunc("/", hello)
-	fmt.Println("listening...")
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-	if err != nil {
-		panic(err)
-	}
-    /*b, err := ioutil.ReadFile("domains.txt") // just pass the file name
-    if err != nil {
-        fmt.Print(err)
-    }
-
-    fmt.Println(b) // print the content as 'bytes'
-
-    str := string(b) // convert content to a 'string'
-
-    fmt.Println(str) // print the content as a 'string'
-}*/
-
-func hello(res http.ResponseWriter, req *http.Request) {
-	 b, err := ioutil.ReadFile("domains.txt") // just pass the file name
-    if err != nil {
-        fmt.Print(err)
-    }
-
-    fmt.Println(b) // print the content as 'bytes'
-
-    str := string(b) // convert content to a 'string'
-
-    fmt.Println(str) // print the content as a 'string'
-
-	fmt.Fprintln(res, str)
- 
+	fmt.Fprintln(res, b)
 }
+
