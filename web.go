194bb43dd613ca3488635cd5cdf44afcfc6e3d9a
avhadpooja <36248454+avhadpooja@users.noreply.github.com>
2018-03-05 14:49:21 -0500

    Update web.go

--- web.go
+++ web.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	//"crypto/md5"
-	"io"
-	//"strconv"
-	//"time"
 	"fmt"
 	"net/http"
 	"os"
-	"html/template"
-	
 )
 
 func main() {
@@ -21,36 +15,10 @@ func main() {
 	}
 }
 
-/*func hello(res http.ResponseWriter, req *http.Request) {
-	t, _ := template.ParseFiles("client.html")
-        t.Execute(res, nil)
+func hello(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(res, "hello, heroku")
 }
-*/
 
-func hello(w http.ResponseWriter, r *http.Request) {
- 
-        t, _ := template.ParseFiles("client.html")
-        t.Execute(w, nil)
-        file, handler, err := r.FormFile("uploadfile")
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
-        defer file.Close()
- 
-        fmt.Fprintf(w, "%v", handler.Header)
-        f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
-        defer f.Close()
- 
-        io.Copy(f, file)
-	fmt.Println(f) 
- 
-}
 
  
 
