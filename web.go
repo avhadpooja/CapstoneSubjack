f594eb35895d8a6651425ae4192f85f9a2b31059
avhadpooja <36248454+avhadpooja@users.noreply.github.com>
2018-03-05 15:13:07 -0500

    Update web.go

--- web.go
+++ web.go
@@ -24,10 +24,30 @@ package main
 import (
     "fmt"
     "io/ioutil"
+    "net/http"
 )
 
 func main() {
-    b, err := ioutil.ReadFile("domains.txt") // just pass the file name
+	http.HandleFunc("/", hello)
+	fmt.Println("listening...")
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	
+    /*b, err := ioutil.ReadFile("domains.txt") // just pass the file name
+    if err != nil {
+        fmt.Print(err)
+    }
+
+    fmt.Println(b) // print the content as 'bytes'
+
+    str := string(b) // convert content to a 'string'
+
+    fmt.Println(str) // print the content as a 'string'
+}*/
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	 b, err := ioutil.ReadFile("domains.txt") // just pass the file name
     if err != nil {
         fmt.Print(err)
     }
@@ -38,7 +58,6 @@ func main() {
 
     fmt.Println(str) // print the content as a 'string'
 }
-
-
+	fmt.Fprintln(res, "hello, heroku")
  
 
