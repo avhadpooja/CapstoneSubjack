0e9ed2dfbaa24dac146ef5bf0a4d7c762217b832
avhadpooja <36248454+avhadpooja@users.noreply.github.com>
2018-03-05 15:03:18 -0500

    Update web.go

--- web.go
+++ web.go
@@ -1,4 +1,4 @@
-package main
+/*package main
 
 import (
 	"fmt"
@@ -17,6 +17,26 @@ func main() {
 
 func hello(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(res, "hello, heroku")
+}*/
+
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+)
+
+func main() {
+    b, err := ioutil.ReadFile("domains.txt") // just pass the file name
+    if err != nil {
+        fmt.Print(err)
+    }
+
+    fmt.Println(b) // print the content as 'bytes'
+
+    str := string(b) // convert content to a 'string'
+
+    fmt.Println(str) // print the content as a 'string'
 }
 
 
