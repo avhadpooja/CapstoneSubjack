ca279c8382d5de3da23ddd5443638dd67781583d
avhadpooja <36248454+avhadpooja@users.noreply.github.com>
2018-03-08 15:48:04 -0500

    Update web.go

--- web.go
+++ web.go
@@ -1,4 +1,4 @@
-/*package main
+package main
 
 import (
 	"fmt"
@@ -24,51 +24,7 @@ func hello(res http.ResponseWriter, req *http.Request) {
         fmt.Print(err)
 			}
 	str := string(b)
-	fmt.Fprintln(res, str) }*/
+	fmt.Fprintln(res, str) 
+}
 	
-package main
-
-import (
-	"fmt"
-	"net/http"
-	"io/ioutil"
-	"os"	
-	"github.com/aws/aws-sdk-go/aws"
-        "github.com/aws/aws-sdk-go/aws/session"
-        "github.com/aws/aws-sdk-go/service/s3"
-)
-
-func main() {
-	http.HandleFunc("/", hello)
-	fmt.Println("listening...")
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-	if err != nil {
-		panic(err)
-	
-			}
-}
-
-func hello(res http.ResponseWriter, req *http.Request) {
-	
-sess := session.Must(session.NewSession())
-// Create a downloader with the session and default options
-downloader := s3manager.NewDownloader(sess)
-
-// Create a file to write the S3 Object contents to.
-f, err := os.Create(filename)
-if err != nil {
-    return fmt.Errorf("failed to create file %q, %v", filename, err)
-}
-
-// Write the contents of S3 Object to the file
-n, err := downloader.Download(f, &s3.GetObjectInput{
-    Bucket: aws.String(bucket),
-    Key:    aws.String(key),
-})
-if err != nil {
-    return fmt.Errorf("failed to download file, %v", err)
-}
-fmt.Printf("file downloaded, %d bytes\n", n)
-	fmt.Fprintln(res, n)	
-}
 
