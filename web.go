684cdcb342ec4d94f554578ff17c55c0fbe5941b
avhadpooja <36248454+avhadpooja@users.noreply.github.com>
2018-03-07 20:20:19 -0500

    Update web.go

--- web.go
+++ web.go
@@ -33,9 +33,9 @@ import (
 	"net/http"
 	"io/ioutil"
 	"os"	
-	//"github.com/aws/aws-sdk-go/aws"
-        //"github.com/aws/aws-sdk-go/aws/session"
-        //"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
+        "github.com/aws/aws-sdk-go/aws/session"
+        "github.com/aws/aws-sdk-go/service/s3"
 )
 
 func main() {
